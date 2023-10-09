5ccb1b27d3bb7685ac93ca14f96ff63260511243
marcoscozatti <marcoscozatti@gmail.com>
2023-10-09 11:28:51 -0300

    ASP.NEet Conecta BD
    ASP.NEet Conecta BD  - primeiro bkp

---
+++ ConectaBancoDadosSQLServer/Controllers/Conecta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConectaBancoDadosSQLServer.Controllers
+{
+    public class Conecta
+    {
+        private static string ConnStr = @"Integrated Security=SSPI;Persist Security Info=False;User ID=sa;Initial Catalog=BDASPNetProjetoConectaBanco;Data Source=DESKTOP-SEV4FT3\SQLEXPRESS";
+
+        public static SqlConnection GetConnection()
+        {
+            var cn = new SqlConnection(ConnStr);
+            return cn;
+        }
+
+        public static void TestaConexao()
+        {
+            try
+            {
+                var connection = Conecta.GetConnection();
+                connection.Open();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+          
+        }
+
+    }
+}
